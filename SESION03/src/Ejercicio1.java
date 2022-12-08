84c8d292d843fc596672a98f9aa5634735e37482
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-12-08 01:42:22 -0500

    some change

--- SESION03/src/Ejercicio1.java
+++ SESION03/src/Ejercicio1.java
@@ -1,14 +1,12 @@
 
-
 public class Ejercicio1 extends javax.swing.JFrame {
 
-    
     public Ejercicio1() {
         initComponents();
     }
 
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
@@ -68,28 +66,33 @@ public class Ejercicio1 extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-    double venta1,venta2,venta3,sueldoMensual,comision,pagoTotal;        // TODO add your handling code here:
-    venta1=Integer.parseInt(venta1Input.getText());
-    venta2=Integer.parseInt(venta2Input.getText());
-    venta3=Integer.parseInt(venta3Input.getText());
-    
-    comision=(venta1+venta2+venta3)*0.10;
-    sueldoMensual=Integer.parseInt(sueldoMensualInput.getText());
-    pagoTotal=sueldoMensual+comision;
-    
-    resultados.setText("El sueldo mensual del vendedor es: "+pagoTotal+"\n"+"La comision recibida es: "+comision);
-       
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton1ActionPerformed
+        double venta1, venta2, venta3, sueldoMensual, comision, pagoTotal; // TODO add your handling code here:
+        venta1 = Integer.parseInt(venta1Input.getText());
+        venta2 = Integer.parseInt(venta2Input.getText());
+        venta3 = Integer.parseInt(venta3Input.getText());
+
+        comision = (venta1 + venta2 + venta3) * 0.10;
+        sueldoMensual = Integer.parseInt(sueldoMensualInput.getText());
+        pagoTotal = sueldoMensual + comision;
+
+        resultados.setText(
+                "El sueldo mensual del vendedor es: " + pagoTotal + "\n" + "La comision recibida es: " + comision);
+
+    }// GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+        // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
+        // (optional) ">
+        /*
+         * If Nimbus (introduced in Java SE 6) is not available, stay with the default
+         * look and feel.
+         * For details see
+         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -99,15 +102,19 @@ public class Ejercicio1 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null,
+                    ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null,
+                    ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null,
+                    ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ejercicio1.class.getName()).log(java.util.logging.Level.SEVERE, null,
+                    ex);
         }
-        //</editor-fold>
+        // </editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
